c7f4315ddab5dbe00c88c5fd16abff9d9ed71f69
And E <348726067@gapps.yrdsb.ca>
2023-11-27 14:03:36 -0500

    Fixed title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -45,7 +45,7 @@ public class TitleScreen extends World
         addObject(label,getWidth()/2,159);
         Label label2 = new Label("Use up Arrow to jump", 30);
         addObject(label2,getWidth()/2,194);
-        Label label4 = new Label("Use Space to jump", 30);
+        Label label4 = new Label("Use Space to dash", 30);
         addObject(label4,getWidth()/2,230);
         Label label3 = new Label("Press <enter> to start", 40);
         addObject(label3,getWidth()/2,373);
