b0c4f77f63fc6d7686575b285211f343e27a42fe
And E <348726067@gapps.yrdsb.ca>
2023-11-26 23:19:39 -0500

    Update TitleScreen.java

--- TitleScreen.java
+++ TitleScreen.java
@@ -25,7 +25,7 @@ public class TitleScreen extends World
     //Main world act loop
     public void act()
     {
-        if(Greenfoot.isKeyDown("space"))
+        if(Greenfoot.isKeyDown("enter"))
         {
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
@@ -45,7 +45,9 @@ public class TitleScreen extends World
         addObject(label,getWidth()/2,159);
         Label label2 = new Label("Use up Arrow to jump", 30);
         addObject(label2,getWidth()/2,194);
-        Label label3 = new Label("Press Space to start", 40);
+        Label label4 = new Label("Use Space to jump", 30);
+        addObject(label4,getWidth()/2,230);
+        Label label3 = new Label("Press <enter> to start", 40);
         addObject(label3,getWidth()/2,373);
     }
 }
